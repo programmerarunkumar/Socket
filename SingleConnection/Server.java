63f5ef4893b0e1e7cef1ece7d13f3293fbbce4c8
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2023-03-12 19:33:41 +0530

    SingleConnection Server

---
+++ SingleConnection/Server.java
@@ -0,0 +1,24 @@
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//https://cs.gmu.edu/~setia/cs571/slides/lec6b.pdf
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Socket clientSocket = serverSocket.accept();
+
+        DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+        String input = dataInputStream.readUTF();
+        System.out.println(input);
+
+        dataInputStream.close();
+        clientSocket.close();
+        serverSocket.close();
+
+    }
+
+}
