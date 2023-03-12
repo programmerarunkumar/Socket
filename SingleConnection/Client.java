2b705fba80bbab1b7ddee16cdf1f9a94b49e9530
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2023-03-12 19:33:07 +0530

    SingleConnection Client

---
+++ SingleConnection/Client.java
@@ -0,0 +1,27 @@
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("localhost", 1234);
+
+        Map data = new HashMap<>();
+        data.put("FROM", "ClientSocket");
+        data.put("TIME", System.currentTimeMillis());
+
+        OutputStream outputStream = socket.getOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeUTF(data.toString());
+        dataOutputStream.flush();
+        dataOutputStream.close();
+
+        socket.close();
+
+    }
+
+}
